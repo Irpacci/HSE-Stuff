0b717464432c3a0ec94a6168ddd58437e9ab9da6
irpacci <asgio2018@gmail.com>
2023-09-13 23:38:18 +0300

    frac 789

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,14 +1,14 @@
 #include "reduce_fraction.h"
 
-int64_t gcd(int64_t a, int64_t b) {
+int64_t Gcd(int64_t a, int64_t b) {
     if (b == 0) {
         return a;
     } else {
-        return gcd(b, a % b);
+        return Gcd(b, a % b);
     }
 }
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    int64_t g = gcd(numerator, denominator);
+    int64_t g = Gcd(numerator, denominator);
     return (numerator + denominator) / g;
 }
