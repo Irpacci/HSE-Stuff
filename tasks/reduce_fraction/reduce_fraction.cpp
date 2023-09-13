620df3f25166c4cfd0fa40414a7e5c645e3d02a0
irpacci <asgio2018@gmail.com>
2023-09-13 23:26:56 +0300

    frac solve

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,6 +1,16 @@
 #include "reduce_fraction.h"
+#include <algorithm>
 
+int64_t gcd (int64_t a, int64_t b) {
+	if (b == 0) {
+        return a;
+    }
+	else {
+        return gcd (b, a % b);
+    }
+}
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    return 1;
+    int64_t g = gcd(numerator, denominator);
+    return (numerator + denominator) / g;
 }
