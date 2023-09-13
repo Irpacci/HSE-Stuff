382b9a031d708f565e2e4eebe89fddb102bd74b3
irpacci <asgio2018@gmail.com>
2023-09-13 14:23:41 +0300

    ii

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,7 +1,12 @@
 #include "is_leap.h"
 
+const int f1 = 4;
+const int f2 = 100;
+const int f3 = 400;
+
 bool IsLeap(int year) {
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+
+    if ((year % f1 == 0 && year % f2 != 0) || year % f3 == 0) {
         return true;
     }
     return false;
