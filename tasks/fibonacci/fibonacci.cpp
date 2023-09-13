465bea5928cc11764a81d3f733a8574ed6dfd5d4
irpacci <asgio2018@gmail.com>
2023-09-13 23:05:40 +0300

    fib 2

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,16 +1,16 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    if (n == 0){
+    if (n == 0) {
         return 0;
     }
-    if (n < 3){
+    if (n < 3) {
         return 1;
     }
     int64_t a = 1;
     int64_t b = 1;
     int64_t c = 1;
-    while (n > 2){
+    while (n > 2) {
         a = b;
         b = c;
         c = a + b;
