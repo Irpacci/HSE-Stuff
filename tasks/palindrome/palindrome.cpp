1451bb2a3186e15b4dd0500ea37df5152fde2bec
irpacci <asgio2018@gmail.com>
2023-09-14 15:48:06 +0300

    try palindrome

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,22 @@
 #include "palindrome.h"
+#include <cstdint>
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    bool answer = true;
+    int64_t l = 0;
+    int64_t r = str.size() - 1;
+    while (l < r) {
+        while (l < str.size() && str[l] == ' ') {
+            l++;
+        }
+        while (r > 0 && str[r] == ' ') {
+            r--;
+        }
+        if (l < r && str[l] != str[r]) {
+            answer = false;
+        }
+        l++;
+        r--;
+    }
+    return answer;
 }
