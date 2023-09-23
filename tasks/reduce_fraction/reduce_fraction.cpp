202140071138f9ae2fe1f4c04860e86765e77c6f
irpacci <asgio2018@gmail.com>
2023-09-23 14:28:06 +0300

    used std::gcd

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,14 +1,7 @@
 #include "reduce_fraction.h"
-
-int64_t Gcd(int64_t a, int64_t b) {
-    if (b == 0) {
-        return a;
-    } else {
-        return Gcd(b, a % b);
-    }
-}
+#include <numeric>
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    int64_t g = Gcd(numerator, denominator);
+    int64_t g = std::gcd(numerator, denominator);
     return (numerator + denominator) / g;
 }
