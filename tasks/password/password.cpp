60e1dec5eaccf71ce5385c24b489764c0638f051
irpacci <asgio2018@gmail.com>
2023-09-23 15:39:34 +0300

    fixed errors

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,21 +1,18 @@
 #include "password.h"
 #include <cstdint>
 
-const int64_t LOWER = 8;
-const int64_t GREATER = 14;
-const int64_t L_ASCII = 33;
-const int64_t R_ASCII = 126;
+constexpr size_t LOWER = 8;
+constexpr size_t GREATER = 14;
+constexpr char L_ASCII = '!';
+constexpr char R_ASCII = '~';
 
 bool ValidatePassword(const std::string& password) {
-    bool checked_size = false;
-    bool checked_codes = true;
-    bool checked_classes = false;
     int64_t has_alpha = 0;
     int64_t has_clpha = 0;
     int64_t has_digit = 0;
     int64_t has_extra = 0;
-    if (password.size() >= LOWER && password.size() <= GREATER) {
-        checked_size = true;
+    if (password.size() < LOWER && password.size() > GREATER) {
+        return false;
     }
     for (auto character : password) {
         if (character >= 'A' && character <= 'Z') {
@@ -27,13 +24,9 @@ bool ValidatePassword(const std::string& password) {
         } else {
             has_extra = 1;
         }
-        if (static_cast<int64_t>(character) < L_ASCII || static_cast<int64_t>(character) > R_ASCII) {
-            checked_codes = false;
+        if (character < L_ASCII || character > R_ASCII) {
+            return false;
         }
     }
-    int64_t sum = has_clpha + has_alpha + has_digit + has_extra;
-    if (sum > 2) {
-        checked_classes = true;
-    }
-    return (checked_classes & checked_codes & checked_size);
+    return (has_clpha + has_alpha + has_digit + has_extra > 2);
 }
