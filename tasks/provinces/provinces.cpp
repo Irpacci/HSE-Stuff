5fdb2eb98981c680c57a3fcb33c11957c7a25f79
irpacci <asgio2018@gmail.com>
2023-10-16 01:38:22 +0300

    fixed naming

--- tasks/provinces/provinces.cpp
+++ tasks/provinces/provinces.cpp
@@ -2,17 +2,17 @@
 
 int64_t CountPassports(const std::vector<int>& provinces) {
     int64_t answer = 0;
-    std::multiset<int64_t> setOfAll;
+    std::multiset<int64_t> set_of_all;
     for (int x : provinces) {
-        setOfAll.insert(x);
+        set_of_all.insert(x);
     }
-    while (setOfAll.size() > 1) {
-        int64_t a = *setOfAll.begin();
-        setOfAll.erase(setOfAll.find(a));
-        int64_t b = *setOfAll.begin();
-        setOfAll.erase(setOfAll.find(b));
+    while (set_of_all.size() > 1) {
+        int64_t a = *set_of_all.begin();
+        set_of_all.erase(set_of_all.find(a));
+        int64_t b = *set_of_all.begin();
+        set_of_all.erase(set_of_all.find(b));
         answer += a + b;
-        setOfAll.insert(a + b);
+        set_of_all.insert(a + b);
     }
     return answer;
 }
