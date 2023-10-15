e6c464f81f7e2ebebfab6fc71b3b9a333fdd28bf
irpacci <asgio2018@gmail.com>
2023-10-16 01:32:53 +0300

    solve provinces

--- tasks/provinces/provinces.cpp
+++ tasks/provinces/provinces.cpp
@@ -1,5 +1,18 @@
 #include "provinces.h"
 
 int64_t CountPassports(const std::vector<int>& provinces) {
-    return 0;
+    int64_t answer = 0;
+    std::multiset<int64_t> setOfAll;
+    for (int x: provinces) {
+        setOfAll.insert(x);
+    }
+    while (setOfAll.size() > 1) {
+        int64_t a = *setOfAll.begin();
+        setOfAll.erase(setOfAll.find(a));
+        int64_t b = *setOfAll.begin();
+        setOfAll.erase(setOfAll.find(b));
+        answer += a + b;
+        setOfAll.insert(a + b);
+    }
+    return answer;
 }
