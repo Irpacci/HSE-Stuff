c6c66c3adabcb9436e3c4fdce5da90cf7e0a0cb9
irpacci <asgio2018@gmail.com>
2023-10-25 23:07:25 +0300

    fix3

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -10,7 +10,7 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     for (size_t i = 0; i < number_of_events; i++) {
         events_order.emplace_back(events[i].time, i);
     }
-    std::map<std::string, std::map<std::string, std::pair<bool, bool>>> 
+    std::map<std::string, std::map<std::string, std::pair<bool, bool>>>
         tasks;  // if last commit is success/if no open merges
     sort(events_order.begin(), events_order.end());
     for (std::pair<time_t, size_t> event : events_order) {
