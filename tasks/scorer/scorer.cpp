4f203a0a6b8e6d70541703307f2446fc1a802bdc
irpacci <asgio2018@gmail.com>
2023-10-25 22:44:09 +0300

    fix codestyle

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -7,17 +7,17 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     ScoreTable answer;
     std::vector<std::pair<time_t, size_t>> events_order;
     size_t number_of_events = events.size();
-    for(size_t i = 0; i < number_of_events; i++) {
+    for (size_t i = 0; i < number_of_events; i++) {
         events_order.emplace_back(events[i].time, i);
     }
     std::map<std::string, std::map<std::string, std::pair<bool, bool>>> tasks; //if last commit is success/if no open merges
     sort(events_order.begin(), events_order.end());
-    for(std::pair<time_t, size_t> event: events_order) {
+    for (std::pair<time_t, size_t> event : events_order) {
         const Event& current_event = events[event.second];
-        if(current_event.time > score_time) {
+        if (current_event.time > score_time) {
             break;
         }
-        switch(current_event.event_type) {
+        switch (current_event.event_type) {
             case EventType::CheckFailed:
                 tasks[current_event.student_name][current_event.task_name].first = 0;
                 break;
@@ -31,8 +31,8 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
                 tasks[current_event.student_name][current_event.task_name].second = 0;
         }
     }
-    for(auto tasks_pair: tasks) {
-        for(auto task_info: tasks_pair.second) {
+    for (auto tasks_pair : tasks) {
+        for (auto task_info : tasks_pair.second) {
             if (task_info.second.first && !task_info.second.second) {
                 answer[tasks_pair.first].insert(task_info.first);
             } 
