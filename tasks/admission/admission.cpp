2f37665dd65b6c53057fdd36765742fd49300286
irpacci <asgio2018@gmail.com>
2023-10-26 05:55:09 +0300

    fixed codestyle 2

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -5,8 +5,8 @@
 #include <map>
 #include "admission.h"
 
-int32_t DAYS_IN_MONTH = 30;
-int32_t DAYS_IN_YEAR = 365;
+const int32_t DAYS_IN_MONTH = 30;
+const int32_t DAYS_IN_YEAR = 365;
 
 int32_t BruteConvertion(const Date& date) {
     return date.day + date.month * DAYS_IN_MONTH + date.year * DAYS_IN_YEAR;
@@ -34,7 +34,7 @@ AdmissionTable FillUniversities(const std::vector<University>& universities, con
     AdmissionTable answer;
     size_t number_of_students = applicants.size();
     std::map<std::string, size_t> max_size;
-    for (auto university : universities) {
+    for (auto const &university : universities) {
         max_size[university.name] = university.max_students;
     }
     std::vector<std::pair<Applicant, size_t>> applicants_sorted;
@@ -42,8 +42,8 @@ AdmissionTable FillUniversities(const std::vector<University>& universities, con
         applicants_sorted.emplace_back(applicants[i], i);
     }
     std::sort(applicants_sorted.begin(), applicants_sorted.end(), ApplicantsComparatorSort);
-    for (auto applicant : applicants_sorted) {
-        for (auto wanted_university : applicant.first.wish_list) {
+    for (auto const &applicant : applicants_sorted) {
+        for (auto const &wanted_university : applicant.first.wish_list) {
             if (answer[wanted_university].size() < max_size[wanted_university]) {
                 answer[wanted_university].push_back(&applicants[applicant.second].student);
                 break;
